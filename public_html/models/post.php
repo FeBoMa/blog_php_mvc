27792fe217e411e3a54287ea8287baf989e3a18d
Ziroks <Ziroks@Lulublu>
2018-11-27 18:43:08 +0100

    Creando insert 2

--- public_html/models/post.php
+++ public_html/models/post.php
@@ -4,14 +4,23 @@ class Post {
 
     // definimos tres atributos
     // los declaramos como públicos para acceder directamente $post->author
+    //title, author, content, created, modified, image
     public $id;
+    public $title;
     public $author;
     public $content;
+    public $created;
+    public $modified;
+    public $image;
 
-    public function __construct($id, $author, $content) {
+    public function __construct($id, $title, $author, $content, $created, $modified, $image) {
         $this->id = $id;
+        $this->title = $title;
         $this->author = $author;
         $this->content = $content;
+        $this->created = $created;
+        $this->modified = $modified;
+        $this->image = $image;
     }
 
     public static function all() {
@@ -38,7 +47,7 @@ class Post {
         return new Post($post['id'], $post['author'], $post['content']);
     }
 
-    public static function insert($title, $author, $content,$image) {
+    public static function insert($title, $author, $content, $image) {
         $db = Db::getInstance();
         // nos aseguramos que $id es un entero
         $req = $db->prepare('INSERT INTO posts (title, author, content, created, modified, image) VALUES (:title, :author, :content, :created, :modified, :image)');
@@ -47,38 +56,104 @@ class Post {
         $req->bindParam(":title", $title);
         $req->bindParam(":author", $author);
         $req->bindParam(":content", $content);
-        $req->bindParam(":created", $created);
-        $req->bindParam(":modified", $modified);
+        $req->bindParam(":created", date('Y-m-d H:i:s'));
+        $req->bindParam(":modified", date('Y-m-d H:i:s'));
         $req->bindParam(":image", $image);
         $req->execute();
     }
-    
+
     public static function update($id, $author, $content) {
         $db = Db::getInstance();
         // nos aseguramos que $id es un entero
         $req = $db->prepare('UPDATE posts SET author = :author, content = :content WHERE id = :id');
 
         // preparamos la sentencia y reemplazamos :id con el valor de $id
-        
+
         $req->bindParam(":id", $id);
         $req->bindParam(":author", $author);
-        $req->bindParam(":content", $content);       
+        $req->bindParam(":content", $content);
         $req->execute();
     }
-    
-    
-    
-        public static function delete($id) {
+
+    public static function delete($id) {
         $db = Db::getInstance();
         // nos aseguramos que $id es un entero
         $req = $db->prepare('DELETE FROM posts WHERE id = :id');
 
         // preparamos la sentencia y reemplazamos :id con el valor de $id
-        $req->bindParam(":id", $id);    
+        $req->bindParam(":id", $id);
         $req->execute();
     }
-    
-    
+
+    function uploadPhoto() {
+
+        $result_message = "";
+
+        // now, if image is not empty, try to upload the image
+        if ($this->image) {
+
+            // sha1_file() function is used to make a unique file name
+            $target_directory = "uploads/";
+            $target_file = $target_directory . $this->image;
+            $file_type = pathinfo($target_file, PATHINFO_EXTENSION);
+
+            // error message is empty
+            $file_upload_error_messages = "";
+            // make sure that file is a real image
+            $check = getimagesize($_FILES["image"]["tmp_name"]);
+            if ($check !== false) {
+                // submitted file is an image
+            } else {
+                $file_upload_error_messages .= "<div>Submitted file is not an image.</div>";
+            }
+
+// make sure certain file types are allowed
+            $allowed_file_types = array("jpg", "jpeg", "png", "gif");
+            if (!in_array($file_type, $allowed_file_types)) {
+                $file_upload_error_messages .= "<div>Only JPG, JPEG, PNG, GIF files are allowed.</div>";
+            }
+
+// make sure file does not exist
+            if (file_exists($target_file)) {
+                $file_upload_error_messages .= "<div>Image already exists. Try to change file name.</div>";
+            }
+
+// make sure submitted file is not too large, can't be larger than 1 MB
+            if ($_FILES['image']['size'] > (1024000)) {
+                $file_upload_error_messages .= "<div>Image must be less than 1 MB in size.</div>";
+            }
+
+// make sure the 'uploads' folder exists
+// if not, create it
+            if (!is_dir($target_directory)) {
+                mkdir($target_directory, 0777, true);
+            }
+            // if $file_upload_error_messages is still empty
+            if (empty($file_upload_error_messages)) {
+                // it means there are no errors, so try to upload the file
+                if (move_uploaded_file($_FILES["image"]["tmp_name"], $target_file)) {
+                    // it means photo was uploaded
+                } else {
+                    $result_message .= "<div class='alert alert-danger'>";
+                    $result_message .= "<div>Unable to upload photo.</div>";
+                    $result_message .= "<div>Update the record to upload photo.</div>";
+                    $result_message .= "</div>";
+                }
+            }
+
+// if $file_upload_error_messages is NOT empty
+            else {
+                // it means there are some errors, so show them to user
+                $result_message .= "<div class='alert alert-danger'>";
+                $result_message .= "{$file_upload_error_messages}";
+                $result_message .= "<div>Update the record to upload photo.</div>";
+                $result_message .= "</div>";
+            }
+        }
+
+        return $result_message;
+    }
+
 }
 
 ?>
