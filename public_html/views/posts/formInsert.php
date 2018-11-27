a2d95155e97862e2cb31867493adc5bf867916bf
Ziroks <Ziroks@Lulublu>
2018-11-27 18:43:44 +0100

    Creando insert 2

--- public_html/views/posts/formInsert.php
+++ public_html/views/posts/formInsert.php
@@ -1,12 +1,20 @@
 <form action="?controller=posts&action=formInsert" method="post" enctype="multipart/form-data">
     <table BORDER=5 BORDERCOLOR=RED>
         <tr>
+            <td>Title</td>
+            <td><input type='text' name='title'/></td>
+        </tr>
+        <tr>
             <td>Author</td>
             <td><input type='text' name='author'/></td>
         </tr>
         <tr>
             <td>Content</td>
             <td><textarea name="content" rows="5" cols="22"></textarea></td>
+        </tr>
+        <tr>
+            <td>Photo</td>
+            <td><input type="file" name="image" /></td>
         </tr>
         <tr>
             <td colspan="2">
